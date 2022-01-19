b617722ad42afd6a8ce830fec652d658dee21b3d
Ahmad Fuad Amsyari <48084093+fuadamsyari@users.noreply.github.com>
2022-01-19 12:49:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// const close = document.querySelector('.close');
+// const card = document.querySelector('.card');
+
+// close.addEventListener('click', function () {
+//   card.style.display = 'none';
+// });
+
+// dom transversal
+// const close = document.querySelectorAll('.close');
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener('click', function (e) {
+//     e.target.parentElement.style.display = 'none';
+//   });
+// }
+// close.forEach((el) => {
+//   el.addEventListener('click', function (e) {
+//     e.target.parentElement.style.display = 'none';
+//   });
+// });
+// close.forEach((el) => {
+//   el.addEventListener('click', function (e) {
+//     e.target.parentElement.style.display = 'none';
+//     e.preventDefault();
+//     e.stopPropagation();
+//   });
+// });
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+  if (e.target.className == 'close') {
+    e.target.parentElement.style.display = 'none';
+    e.preventDefault();
+  }
+});
